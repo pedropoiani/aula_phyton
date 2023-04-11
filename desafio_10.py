72b30c4741560fcfd8162bc6a8af8381c08e8eb0
Pedro Henrique Poiani <pedropoiani@gmail.com>
2023-04-11 00:22:09 -0300

    Add files via upload

---
+++ desafio_10.py
@@ -0,0 +1,5 @@
+#Faça um programa que lcia um número Inteiro qualquer e mostra na tela a sua tabuada.
+
+num=int(input('Digite um numero: '))
+
+print(f'A sua tabuada é\n{num*1}\n{num*2}\n{num*3}\n{num*4}\n...\n{num*9}\n{num*10}')
