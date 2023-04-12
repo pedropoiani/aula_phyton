097c8ac5bdfe2200be9978953105b6a092b23573
Pedro Henrique Poiani <pedropoiani@gmail.com>
2023-04-12 13:40:33 -0300

    Add files via upload

--- desafio_11.py
+++ desafio_11.py
@@ -1,5 +1,6 @@
-#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira G mostra quantos Dólares ela pode comprar.
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira G 
+#mostra quantos Dólares ela pode comprar.
 
 saldo=float(input('Quanto de real você tem? '))
 cota=float(input('Qual a cotação atual do dolar? '))
-print(f'Seu saldo em dolar é ${saldo/cota:.3f}')
+print(f'Seu saldo em dolar é ${saldo/cota:.2f}')
