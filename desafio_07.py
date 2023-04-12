e8c97143bd4d96154fbcbd76b525362ba820ea3c
Pedro Henrique Poiani <pedropoiani@gmail.com>
2023-04-12 09:09:45 -0300

    limitando float

--- desafio_07.py
+++ desafio_07.py
@@ -1,5 +1,3 @@
 #Cria um algoritmo que leia um número a mostra o seu dobro. triplo a raiz quadrada.
-
 num=int(input('Digite um Numero: '))
-
-print(f'Seu Dobro é {num*2}\nSeu Triplo é {num*3}\nSua Raiz Quadrada é {num**(1/2)}')
+print(f'Seu Dobro é {num*2}\nSeu Triplo é {num*3}\nSua Raiz Quadrada é {num**(1/2):.2f}')
