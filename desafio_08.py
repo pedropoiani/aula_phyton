8b54c480e007134587e0138e4f4ec18044c39f8e
Pedro Henrique Poiani <pedropoiani@gmail.com>
2023-04-12 09:13:45 -0300

    nova variável

--- desafio_08.py
+++ desafio_08.py
@@ -2,4 +2,5 @@
 
 nota1=float(input('Qual a nota 1: '))
 nota2=float(input('Qaul a nota 2: '))
-print(f'A média desse aluno é: {(nota1+nota2)/2}')
+media= (nota1 + nota2)/2
+print(f'A média desse aluno é: {media:.1f}')
