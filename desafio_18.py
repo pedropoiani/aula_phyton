c7d10b1aea13125f296bc47a0ba0b21efe56b7f9
Pedro Henrique Poiani <pedropoiani@gmail.com>
2023-04-17 16:36:58 -0300

    Add files via upload

---
+++ desafio_18.py
@@ -0,0 +1,8 @@
+#faça um programa que leia um angulo e calcule
+#Para encontrar os valores do seno, cosseno e tangente
+
+import math
+angulo = float(input('Qual o Angulo?'))
+print(f' Seno: {math.sin(angulo)}')
+print(f' Cosseno: {math.cos(angulo)}')
+print(f' Tangente: {math.tan(angulo)}')
